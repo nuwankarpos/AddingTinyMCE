f0b02792cde6710d3b97ae1e0e2261820c55df68
Nuwan <nuwan@karposconsultants.com>
2019-10-22 12:42:38 +0530

    Add project files.

---
+++ AddingTinyMCE/Controllers/ContactManagerController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AddingTinyMCE.Entities;
+
+namespace AddingTinyMCE.Controllers
+{
+    public class ContactManagerController : Controller
+    {
+        private ContactDBEntities db = new ContactDBEntities();
+
+        //
+        // GET: /ContactManager/
+
+        public ActionResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        //
+        // GET: /ContactManager/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /ContactManager/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /ContactManager/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        //
+        // GET: /ContactManager/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /ContactManager/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /ContactManager/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /ContactManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
